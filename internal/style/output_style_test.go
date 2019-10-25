7b71eacb44ad41f4f6d2c6993c23a56a26f2dcf0
Corentin de Boisset <corentin.deboisset@gmail.com>
2019-10-25 18:29:17 +0200

    add a test on an invalid style

--- internal/style/output_style_test.go
+++ internal/style/output_style_test.go
@@ -20,3 +20,12 @@ func TestApplyStyle(t *testing .T) {
 
 	assertEqualsString(t, mystyle.Apply("This is a text."), "\033[1;32;41mThis is a text.\033[22;39;49m")
 }
+
+// TestApplyInvalidStyle checks that invalid style properties will not be applied
+func TestApplyInvalidStyle(t *testing.T) {
+	mystyle := OutputStyle{Foreground: "blau", Background: "rojo", Options: []string{"gras"}}
+	assertEqualsString(t, mystyle.Apply("This is a text."), "This is a text.")
+
+	mystyle = OutputStyle{Foreground: "blue", Background: "rojo", Options: []string{"gras"}}
+	assertEqualsString(t, mystyle.Apply("This is a text."), "\033[34mThis is a text.\033[39m")
+}
