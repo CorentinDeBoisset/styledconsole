8c61870d7d176ea70f55e34f44ea7902412ee9b2
Corentin de Boisset <corentin.deboisset@gmail.com>
2019-10-31 21:12:19 +0100

    use testify for assertions

--- internal/style/output_style_test.go
+++ internal/style/output_style_test.go
@@ -1,31 +1,34 @@
 package style
 
 import (
-	"fmt"
-	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
-
-func assertEqualsString(t *testing.T, a string, b string) {
-	if a == b {
-		return
-	}
-
-	t.Error(fmt.Sprintf("Assertion failed:\n  %v != %v", strconv.QuoteToASCII(a), strconv.QuoteToASCII(b)))
-}
 
 // TestApplyStyle checks that we can apply a test on a string
 func TestApplyStyle(t *testing.T) {
 	mystyle := OutputStyle{Foreground: "green", Background: "red", Options: []string{"bold"}}
-
-	assertEqualsString(t, mystyle.Apply("This is a text."), "\033[1;32;41mThis is a text.\033[22;39;49m")
+	assert.Equal(
+		t,
+		"\033[1;32;41mThis is a text.\033[22;39;49m",
+		mystyle.Apply("This is a text."),
+	)
 }
 
 // TestApplyInvalidStyle checks that invalid style properties will not be applied
 func TestApplyInvalidStyle(t *testing.T) {
 	mystyle := OutputStyle{Foreground: "blau", Background: "rojo", Options: []string{"gras"}}
-	assertEqualsString(t, mystyle.Apply("This is a text."), "This is a text.")
+	assert.Equal(
+		t,
+		"This is a text.",
+		mystyle.Apply("This is a text."),
+	)
 
 	mystyle = OutputStyle{Foreground: "blue", Background: "rojo", Options: []string{"gras"}}
-	assertEqualsString(t, mystyle.Apply("This is a text."), "\033[34mThis is a text.\033[39m")
+	assert.Equal(
+		t,
+		"\033[34mThis is a text.\033[39m",
+		mystyle.Apply("This is a text."),
+	)
 }
